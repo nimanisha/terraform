8355d28f718e332428e7c368c457f67a356fe629
Nimanisha <135317702+nimanisha@users.noreply.github.com>
2023-06-26 15:19:45 +0330

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -25,14 +25,14 @@ vpc_security_group_ids = ["output.sgid"]
 resource "aws_security_group" "ec2-sg"{
 name = "myec2sg"
 ingress {
-from_port="0"
-to_port="65536"
+from_port="22"
+to_port="22"
 protocol = "tcp"
 cidr_blocks=["0.0.0.0/0"]
 }
 egress {
-from_port="0"
-to_port="65536"
+from_port="22"
+to_port="22"
 protocol = "tcp"
 cidr_blocks=["0.0.0.0/0"]
 }
