046f8fd9e5297b7e1693d2b5fec93b4227b6dfd5
Nimanisha <135317702+nimanisha@users.noreply.github.com>
2023-06-26 15:31:11 +0330

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -2,7 +2,7 @@ provider "aws"{
 region = "us-east-1"
 }
 
-resource "aws_instance" "mec2"{
+resource "aws_instance" "mec2" {
 ami = "ami-00183ea8ca7298292"
 instance_type = "t2.micro"
 key_name="ec2"
